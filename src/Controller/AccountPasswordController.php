45404f8f452664a47c6e2b28ed4b65bb8bffdbff
saad <rs.mailpro@gmail.com>
2022-05-18 08:44:11 +0200

    Formulaire de modification de mot de passe OK

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -4,17 +4,47 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ChangePasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AccountPasswordController extends AbstractController
 {
-    #[Route('/account/modifier-mon-mot-de-passe', name: 'app_account_password')]
-    public function index(): Response
+
+    private $entityManager;
+
+    public function __construct(ManagerRegistry $doctrine){
+        $this->entityManager = $doctrine->getManager();
+    }
+
+    #[Route('/compte/modifier-mon-mot-de-passe', name: 'app_account_password')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){ // SI le formulaire a été soumis e
+            // Nous allons comparer le mot de passe saisi par le mot de passe en base
+
+            $old_pwd = $form->get('old_password')->getData();
+
+            if($passwordHasher->isPasswordValid($user, $old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+
+                // encodage du nouveau mot de passe
+                $hashedPassword = $passwordHasher->hashPassword($user, $new_pwd);
+                $user->setPassword($hashedPassword);
+                // Pas besoin d'utiliser le methode persist puisque l'objet user est déjà géré par doctrine
+                $this->entityManager->flush($user);
+            }
+
+        }
+
 
         return $this->render('account/password.html.twig', [
             'form' => $form->createView()
